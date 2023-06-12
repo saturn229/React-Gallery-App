ce73e6b5ea9409642c591995da75caa5776fdf2f
saturn229 <30321956+saturn229@users.noreply.github.com>
2023-06-12 15:11:42 -0500

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,8 +6,8 @@ const Nav = () => (
     <nav className="main-nav">
         <ul>
             <li><NavLink to='/Bees'>Bees</NavLink></li>
-            <li><NavLink to='/Space'>Space</NavLink></li>
-            <li><NavLink to='/Art'>Art</NavLink></li>
+            <li><NavLink to='/Sunset'>Sunset</NavLink></li>
+            <li><NavLink to='/Cats'>Cats</NavLink></li>
         </ul>
     </nav>
 );
